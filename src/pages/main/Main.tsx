746970abcd4a91f5da408795d59a4da8ffbff11c
sabanaite <sabanaite@gmail.com>
2023-12-10 22:07:37 +0200

    refactor

---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Article from "../../components/article/Article";
+import { reasons } from '../../service/ArticleService';
+import Body from '../../layouts/main-layout/body/Body';
+
+const Main: React.FC = () => (
+    <Body>
+        <div className={"body-container"}>
+            {
+                reasons.map((reason) =>
+                    <Article id={reason.id} title={reason.title} description={reason.description}/>
+                )
+            }
+        </div>
+    </Body>
+)
+
+export default Main;
