e47cd5418a0408a1f65cbdcc05fdbdcff2d1372b
magma <dariusnavickasdn@gmail.com>
2023-12-27 02:44:07 +0200

    edit footer content

--- src/layouts/main-layout/footer/Footer.tsx
+++ src/layouts/main-layout/footer/Footer.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 import logo from '../../../static/reasonstoswallowlogo.png';
 import './Footer.scss';
+import {Link} from "react-router-dom";
 
 const Footer = () => (
     <footer className="footer">
-       <div className="footer-item col-3">
-            this is footer
-        </div>
-        <div className="footer-item col-3">
-            <img  src={logo} alt="reasons to"/>
+        <div className="header-item col-3 logo">
+            <Link to="/" >
+                <img  src={logo} alt="reasons to"/>
+                ReasonsToSwallow
+            </Link>
         </div>
     </footer>
 );
